fc5a36d3f51aa0db4ecd456c56876c2cc42f26d3
cisiqo <csq1124@gmail.com>
2013-01-12 23:15:00 +0800

    update database connection

--- cal.php
+++ cal.php
@@ -1,11 +1,17 @@
 <?php
+define('SABRE_MYSQLDSN','mysql:host=127.0.0.1;dbname=baikal');
+define('SABRE_MYSQLUSER','root');
+define('SABRE_MYSQLPASS','111111');
 
 $baseURI = str_replace("\\","/",str_replace(__DIR__,"",__FILE__));
 
+$pdo = new \PDO(SABRE_MYSQLDSN,SABRE_MYSQLUSER,SABRE_MYSQLPASS);
+
+require_once "SabreDAV/vendor/autoload.php";
 # Backends
-$authBackend = new \Sabre\DAV\Auth\Backend\PDO($GLOBALS["DB"]->getPDO());
-$principalBackend = new \Sabre\DAVACL\PrincipalBackend\PDO($GLOBALS["DB"]->getPDO());
-$calendarBackend = new \Sabre\CalDAV\Backend\PDO($GLOBALS["DB"]->getPDO());
+$authBackend = new \Sabre\DAV\Auth\Backend\PDO($pdo);
+$principalBackend = new \Sabre\DAVACL\PrincipalBackend\PDO($pdo);
+$calendarBackend = new \Sabre\CalDAV\Backend\PDO($pdo);
 
 # Directory structure
 $nodes = array(
@@ -15,10 +21,10 @@ $nodes = array(
 
 # Initializing server
 $server = new \Sabre\DAV\Server($nodes);
-$server->setBaseUri($baseURI);
+$server->setBaseUri("/cal.php/");
 
 # Server Plugins
-$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend,"calsync"));
+$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend,"BaikalDAV"));
 $server->addPlugin(new \Sabre\DAVACL\Plugin());
 $server->addPlugin(new \Sabre\CalDAV\Plugin());
 
